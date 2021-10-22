4824125d13a81b0985618f426ee9d95bc0a17810
Rafu Defelice <defelicerafael@gmail.com>
2021-10-21 22:05:21 -0300

    Quienes-somos

---
+++ templates/quienes-somos.php
@@ -0,0 +1,182 @@
+<?php
+$quienes[] = array(
+    "img"=>"../img/quienes-somos/Javier_de_la_Vega.png",
+    "nombre"=>"Javier de la Vega",
+    "linkedin"=>"https://linkedin.com",
+    "puesto"=>"CEO – FUNDADOR",
+    "desc"=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean id auctor libero."
+);
+$quienes[] = array(
+    "img"=>"../img/quienes-somos/Ariel_Tarnaruder.png",
+    "nombre"=>"Ariel Tarnaruder",
+    "linkedin"=>"https://linkedin.com",
+    "puesto"=>"CCO – FUNDADOR",
+    "desc"=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean id auctor libero."
+);
+$quienes[] = array(
+    "img"=>"../img/quienes-somos/Agustin_Pastoriza.png",
+    "nombre"=>"Agustín Pastoriza",
+    "linkedin"=>"https://linkedin.com",
+    "puesto"=>"CTO   – FUNDADOR",
+    "desc"=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean id auctor libero."
+);
+$quienes[] = array(
+    "img"=>"../img/quienes-somos/Juan_Pablo_Quinteros.png",
+    "nombre"=>"Juan Pablo Quinteros",
+    "linkedin"=>"https://linkedin.com",
+    "puesto"=>"CCO – SOCIO",
+    "desc"=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean id auctor libero."
+);
+$quienes[] = array(
+    "img"=>"../img/quienes-somos/Damian_Salimbene.png",
+    "nombre"=>"Damian Salimbene",
+    "linkedin"=>"https://linkedin.com",
+    "puesto"=>"CONTROLLER – SOCIO",
+    "desc"=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean id auctor libero."
+);
+$quienes[] = array(
+    "img"=>"../img/quienes-somos/Juan_Pablo_Navone.png",
+    "nombre"=>"Juan Pablo Navone",
+    "linkedin"=>"https://linkedin.com",
+    "puesto"=>"CPO",
+    "desc"=>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean id auctor libero."
+);
+?>
+
+
+<div class="row g-0 align-items-center taxmotor-padding" id="quienes-1">
+    <div class="col-md-6 px-md-3 px-3">
+        <h3 class="letra-tus-impuestos gris-oscuro">
+            Acerca de nosotros
+        </h3>
+        <p class="letra-20"> 
+            Queremos hacerte la vida más fácil con tus impuestos 
+        </p>
+        <p class="os-regular">
+        Seguramente los impuestos sean un dolor de cabeza para vos, como para la mayoría. Pagas un montón de plata, no sabes bien por qué. Aparecen cosas imprevistas, nadie te avisa nada, no podés hacer nada. O por ahí directamente no te gustan los impuestos, no es lo tuyo, te dan miedo. 
+        <br/><br/>
+        Así que terminás haciendo lo que te dicen. Pagás y listo. Y sentís que podría haber sido distinto.
+        <br/><br/>
+        Seas un empleado, monotributista, un profesional o un emprendedor, estamos para ayudarte a administrar y liquidar tus impuestos vos mismo, bien hechos. Y sin pagar de más.
+        <br/><br/>
+        Con la tecnología de taxmotor tomás el control de tus impuestos, sin necesidad de ser contador. Podés armar tu planificación fiscal, con nuestras herramientas exclusivas. Tenés tu agenda personal y notificaciones para que estés informado y no se te pase nada. Guardás toda tu documentación, segura y ordenada en un solo lugar, al que solo 
+        </p>
+    </div>
+    <div class="col-md-6">
+        <img src="../img/quienes-somos/07_Quienes-Somos_FOTO.jpg" alt="" title="" class="w-100">
+    </div>
+    <div class="col-12 mt-md-5 px-md-3 px-3 mt-md-3 mt-3">
+        <h3 class="letra-tus-impuestos secundario text-center">
+            Los Valores de Taxmotor
+        </h3>
+        <p class="letra-20 text-center"> 
+            Creemos mucho en los valores. Nos ayudan a funcionar, a tomar 
+            <br class="d-none d-sm-none d-md-block"/>
+            decisiones en el día a día. Trabajamos para hacerlos carne en nuestro equipo
+        </p>
+    </div>
+</div>
+<div class="row g-0 taxmotor-padding" id="quienes-2">    
+    <div class="col-md-6 col-12  pe-md-5 ps-3 ps-ms-0">
+        <p>
+            <span>
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="6" height="10" viewBox="0 0 6 10">
+                    <image id="FLECHA" width="6" height="10" xlink:href="data:img/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAYAAAAKBAMAAABlIDIAAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAMFBMVEVAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeMAAAAr8WgyAAAADnRSTlMAwJbPpQYt7Q8/+cwMWqwkcYMAAAABYktHRA8YugDZAAAACXBIWXMAAAsSAAALEgHS3X78AAAAB3RJTUUH5QoSEhwQ3p+dAwAAACVJREFUCNdjEGJgYLBzZWBIf9jAwLluDwPDvUdgDGKDxEByQDUA2+wLIR2xaqcAAAAASUVORK5CYII="/>
+                </svg>
+            </span>
+            <span class="pbold secundario">Clientes contentos</span>
+            <br/>
+            <span class="gris-oscuro os-regular">
+                Todo lo que hacemos es para que nuestros clientes puedan administrar y planificar sus impuestos de forma fácil, segura y al menor costo. Queremos clientes que estén tranquilos, que se queden con nosotros por años, y que nos recomienden. Estamos siempre escuchándolos.
+            </span>
+        </p>
+        <p>
+            <span>
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="6" height="10" viewBox="0 0 6 10">
+                    <image id="FLECHA" width="6" height="10" xlink:href="data:img/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAYAAAAKBAMAAABlIDIAAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAMFBMVEVAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeMAAAAr8WgyAAAADnRSTlMAwJbPpQYt7Q8/+cwMWqwkcYMAAAABYktHRA8YugDZAAAACXBIWXMAAAsSAAALEgHS3X78AAAAB3RJTUUH5QoSEhwQ3p+dAwAAACVJREFUCNdjEGJgYLBzZWBIf9jAwLluDwPDvUdgDGKDxEByQDUA2+wLIR2xaqcAAAAASUVORK5CYII="/>
+                </svg>
+            </span>
+            <span class="pbold secundario">Innovación</span>
+            <br/>
+            <span class="gris-oscuro os-regular">
+                Asumimos el compromiso de innovar a través de la tecnología para brindarle a nuestros clientes la mejor experiencia. Estamos siempre pensando en cómo automatizar y digitalizar todos los procesos.
+            </span>
+        </p>
+    </div>
+    <div class="col-md-6 col-12 pe-md-5 ps-3 ps-ms-0">
+        <p>
+            <span>
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="6" height="10" viewBox="0 0 6 10">
+                    <image id="FLECHA" width="6" height="10" xlink:href="data:img/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAYAAAAKBAMAAABlIDIAAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAMFBMVEVAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeMAAAAr8WgyAAAADnRSTlMAwJbPpQYt7Q8/+cwMWqwkcYMAAAABYktHRA8YugDZAAAACXBIWXMAAAsSAAALEgHS3X78AAAAB3RJTUUH5QoSEhwQ3p+dAwAAACVJREFUCNdjEGJgYLBzZWBIf9jAwLluDwPDvUdgDGKDxEByQDUA2+wLIR2xaqcAAAAASUVORK5CYII="/>
+                </svg>
+            </span>
+            <span class="pbold secundario">Libertad y responsabilidad</span>
+            <br/>
+            <span class="gris-oscuro os-regular">
+                Asumimos el compromiso de innovar a través de la tecnología para brindarle a nuestros clientes la mejor experiencia. Estamos siempre pensando en cómo automatizar y digitalizar todos los procesos.
+            </span>
+        </p>
+        <p>
+            <span>
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="6" height="10" viewBox="0 0 6 10">
+                    <image id="FLECHA" width="6" height="10" xlink:href="data:img/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAYAAAAKBAMAAABlIDIAAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAMFBMVEVAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeMAAAAr8WgyAAAADnRSTlMAwJbPpQYt7Q8/+cwMWqwkcYMAAAABYktHRA8YugDZAAAACXBIWXMAAAsSAAALEgHS3X78AAAAB3RJTUUH5QoSEhwQ3p+dAwAAACVJREFUCNdjEGJgYLBzZWBIf9jAwLluDwPDvUdgDGKDxEByQDUA2+wLIR2xaqcAAAAASUVORK5CYII="/>
+                </svg>
+            </span>
+            <span class="pbold secundario">Resultados</span>
+            <br/>
+            <span class="gris-oscuro os-regular">
+                Estamos 100% orientados a resultados. Medimos, analizamos y accionamos para alcanz ar nuestros objetivos.
+            </span>
+        </p>
+        <p>
+            <span>
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="6" height="10" viewBox="0 0 6 10">
+                    <image id="FLECHA" width="6" height="10" xlink:href="data:img/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAYAAAAKBAMAAABlIDIAAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAMFBMVEVAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeNAeeMAAAAr8WgyAAAADnRSTlMAwJbPpQYt7Q8/+cwMWqwkcYMAAAABYktHRA8YugDZAAAACXBIWXMAAAsSAAALEgHS3X78AAAAB3RJTUUH5QoSEhwQ3p+dAwAAACVJREFUCNdjEGJgYLBzZWBIf9jAwLluDwPDvUdgDGKDxEByQDUA2+wLIR2xaqcAAAAASUVORK5CYII="/>
+                </svg>
+            </span>
+            <span class="pbold secundario">Pasarla bien</span>
+            <br/>
+            <span class="gris-oscuro os-regular">
+            Trabajamos duro, en equipo, en un ambiente de respeto, apoyo, compromiso, comunicación, ganas de aprender y enseñar, ganas de hacer y de crecer, con motivación y buena onda.
+            </span>
+        </p>
+    </div>    
+</div> 
+<div class="row g-0 taxmotor-padding" id="quienes-3">  
+    <div class="col-md-12 px-md-5 px-5 text-center">
+        <h3 class="letra-tus-impuestos gris-oscuro">
+            Somos personas
+        </h3>
+        <p class="letra-20"> 
+            Vivimos en un mundo cada vez más digital, donde se accede a la información en solo click. Todo se hacemos
+            <br class="d-none d-sm-none d-md-block"/>
+            más fácil y a menor costo. Somos una empresa de tecnología, pero queremos que siempre te sientas 
+        </p>
+    </div>
+    <div class="row row-cols-2 row-cols-md-3 g-0 mt-md-5 mt-4 poppins p-md-0 p-2">
+        <?php
+            foreach($quienes as $k=>$v){
+        ?>
+            <div class="col align-self-center mb-4 px-md-5 px-3 ">
+                <img src="<?php echo $v['img'];?>" class="mx-auto d-block" alt="...">
+                <div class="card-title text-center pt-3">
+                    <p>
+                        <a href="<?php echo $v['link'];?>" alt="" class="gris-oscuro" style="text-decoration:none"> 
+                            <?php echo $v['nombre'];?>
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="17" height="17" viewBox="0 0 17 17">
+                                <image id="ICONO_LINKEDIN" data-name="ICONO LINKEDIN" width="17" height="17" xlink:href="data:img/png;base64,iVBORw0KGgoAAAANSUhEUgAAABEAAAARCAMAAAAMs7fIAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAOVBMVEU+d+r///8+d+o+d+o+d+o+d+o+d+o+d+o+d+o+d+o+d+o+d+o+d+o+d+o+d+o+d+o+d+o+d+oAAAAnVQcvAAAAEXRSTlMAAEAwcIDPv4+fYO+vIFDfEDOpJU0AAAABYktHRBJ7vGwAAAAACXBIWXMAAAsSAAALEgHS3X78AAAAB3RJTUUH5QoVFTQS9VR8uQAAAF1JREFUGNOlzEEOgDAIRNEpCmir1rn/ZcUYk+LWvxreAkBSU8HMnEBJs0E0xIElywrULNTaPmJOutF0ewXw+FUBSJJdJcYozoa4BuE9fskhvcsRMsv2SKriXHxMywXMLgsMA2+wXgAAAABJRU5ErkJggg=="/>
+                            </svg>
+                        </a>    
+                        <br/>
+                        <span class="secundario"><?php echo $v['puesto'];?></span>
+                    </p>
+                    <p class="px-md-3 os-regular"><?php echo $v['desc'];?></p>
+                </div>
+               
+            </div>
+        <?php
+        }
+        ?>
+        
+    </div>     
+</div>    
