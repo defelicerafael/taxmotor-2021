86ee26cb91ee4c20f2095a56fcde474a312e169a
Rafu Defelice <defelicerafael@gmail.com>
2021-10-25 15:06:08 -0300

    Con efecto footer

--- index.php
+++ index.php
@@ -25,24 +25,27 @@ include_once 'server/link.php';
         <meta property="og:image"         content="<?php echo $link;?>img/seo.jpg" />
     </head>
     <body>
-        <div class="container-taxmotor">
-          <div class="bg-primario">
+          <div class="container-taxmotor">  
             <?php include_once "templates/top-nav-comun.php";?>
-            <?php include_once "templates/video.php";?>
-            <?php include_once "templates/probalo.php";?>
-            <?php include_once "templates/top-nav-comun.php";?>    
-            <?php include_once "templates/sin-dolor-de-cabeza.php";?>
-            <?php include_once "templates/top-nav-comun.php";?>   
-            <?php include_once "templates/planificacion-fiscal.php";?>
-            <?php include_once "templates/experto.php";?>
-            <?php include_once "templates/tus-datos.php";?>
-            <?php include_once "templates/empeza-a-gestionar.php";?>
-            <?php include_once "templates/satisfaccion-garantizada.php";?>
-            <?php include_once "templates/empresas.php";?>
-            <?php include_once "templates/toma-el-control.php";?>
-          </div>
-            <?php include_once "templates/footer.php";?>    
-        </div>
+            <div class="main bg-primario">  
+              <?php include_once "templates/video.php";?>
+              <?php include_once "templates/probalo.php";?>
+              <?php include_once "templates/top-nav-comun.php";?>    
+              <?php include_once "templates/sin-dolor-de-cabeza.php";?>
+              <?php include_once "templates/top-nav-comun.php";?>   
+              <?php include_once "templates/planificacion-fiscal.php";?>
+              <?php include_once "templates/experto.php";?>
+              <?php include_once "templates/tus-datos.php";?>
+              <?php include_once "templates/empeza-a-gestionar.php";?>
+              <?php include_once "templates/satisfaccion-garantizada.php";?>
+              <?php include_once "templates/empresas.php";?>
+              <?php include_once "templates/toma-el-control.php";?>
+            </div>
+            <div class="footer-efect bg-primario">
+              <?php include_once "templates/footer.php";?>    
+            </div>
+          </div>   
+          
         <!-- Option 1: Bootstrap Bundle with Popper -->
         <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
 
--- templates/footer.php
+++ templates/footer.php
@@ -1,8 +1,13 @@
-<div class="footer-efect">    
+<div class="">    
     <div class="taxmotor-padding bg-light" id="footer">
         <div class="row g-0  py-md-4 py-3">
             <div class="col-md-3 col-12">
                 <img src="<?php echo $link;?>img/logo/taxmotor-240.png" alt="" class="padding-foto-footer img-fluid mx-auto d-block mx-md-0 py-4 py-md-0">   
+                <div class="row g-0">
+                    <div class="col"></div>
+                    <div class="col"></div>
+                    <div class="col"></div>
+                </div>
             </div>
             <div class="col-md-7 col-12 px-md-3 px-3">
                 <div class="row g-0">
