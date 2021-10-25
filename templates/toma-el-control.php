7ff1bdb86b710c229b5f132273e7aab5a2639c00
Rafu Defelice <defelicerafael@gmail.com>
2021-10-25 14:13:54 -0300

    css

--- templates/toma-el-control.php
+++ templates/toma-el-control.php
@@ -1,6 +1,6 @@
 <div class="bg-oscuro" id="tomaelcontrol">
     <div class="row g-0 align-items-center py-md-4 py-4 px-3 px-md-0">
-        <div class="col-md-7">
+        <div class="col-md-7 col-12">
             <h2 class="letra-36 text-white text-md-end text-center padding-end">
                 Tomá el control de tus impuestos.
                 <br/>
@@ -8,12 +8,12 @@
             </h2>
             
         </div>
-        <div class="col-md-5 pt-3 pt-md-0">
-            <div class="row g-0 px-md-5 ">
-                <div class="col ps-md-3 px-3 px-md-0">
+        <div class="col-md-5 col-12 pt-3 pt-md-0">
+            <div class="row g-0 justify-content-center px-md-5 text-center">
+                <div class="col ps-md-3 px-md-0">
                     <button class="btn bg-secundario text-white btn-config-4">PERSONAS</button>
                 </div>
-                <div class="col px-3 px-md-0">
+                <div class="col px-md-0">
                     <button class="btn bg-secundario text-white btn-config-4">&nbsp;&nbsp;PYMES&nbsp;</button>
                 </div>
             </div>
