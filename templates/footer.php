923db9b6fa7c468389879a80d1a67bc8eaa88c5f
Rafu Defelice <defelicerafael@gmail.com>
2021-10-25 16:43:46 -0300

    footer ok

--- templates/footer.php
+++ templates/footer.php
@@ -1,12 +1,19 @@
 <div class="">    
     <div class="taxmotor-padding bg-light" id="footer">
         <div class="row g-0  py-md-4 py-3">
-            <div class="col-md-3 col-12">
+            <div class="col-md-3 col-12 text-center text-md-start">
                 <img src="<?php echo $link;?>img/logo/taxmotor-240.png" alt="" class="padding-foto-footer img-fluid mx-auto d-block mx-md-0 py-4 py-md-0">   
-                <div class="row g-0">
-                    <div class="col"></div>
-                    <div class="col"></div>
-                    <div class="col"></div>
+                <h5 class="os-bold ps-md-3 margen-alto-seguinos py-md-0 pt-2">Seguinos:</h5>
+                <div class="row g-0 justify-content-center justify-content-md-start ps-md-3 pb-md-0 pb-4">
+                    <div class="col-md-2 col-2">
+                        <img src="<?php echo $link;?>img/home/facebook.png" alt="" title="">
+                    </div>
+                    <div class="col-md-2 col-1">
+                        <img src="<?php echo $link;?>img/home/instagram.png" alt="" title="">
+                    </div>
+                    <div class="col-md-2 col-2">
+                        <img src="<?php echo $link;?>img/home/twitter.png" alt="" title="">
+                    </div>
                 </div>
             </div>
             <div class="col-md-7 col-12 px-md-3 px-3">
